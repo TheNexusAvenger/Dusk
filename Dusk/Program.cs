65ad2e7f9da5e10b0989437dade9f5cdbb745ff7
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2026-03-24 11:46:41 -0400

    Create initial application.

---
+++ Dusk/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.CommandLine;
+using Dusk.Diagnostic;
+using Microsoft.Extensions.Logging;
+
+namespace Dusk;
+
+public class Program
+{
+    /// <summary>
+    /// Command option for debug logging.
+    /// </summary>
+    public static readonly Option<bool> DebugOutputOption = new Option<bool>("--debug")
+    {
+        Description = "Enables debug logging.",
+    };
+    
+    /// <summary>
+    /// Runs the program.
+    /// </summary>
+    /// <param name="args">Arguments from the command line.</param>
+    public static void Main(string[] args)
+    {
+        // Create the command for running the server.
+        var serveCommand = new Command("serve", description: "Runs the server.");
+        serveCommand.Options.Add(DebugOutputOption);
+        serveCommand.SetAction(parseResult =>
+        {
+            // TODO
+        });
+        
+        // Create the command for running the client.
+        var runCommand = new Command("run", description: "Runs the client.");
+        runCommand.Options.Add(DebugOutputOption);
+        runCommand.SetAction(parseResult =>
+        {
+            // TODO
+        });
+        
+        // Create the command for sending the current clipboard to the server.
+        var sendClipboardCommand = new Command("send-clipboard", description: "Sends the current clipboard to the server.");
+        sendClipboardCommand.Options.Add(DebugOutputOption);
+        sendClipboardCommand.SetAction(parseResult =>
+        {
+            // TODO
+        });
+        
+        // Create the root command.
+        var rootCommand = new RootCommand(description: "Synchronizes remote systems.");
+        rootCommand.Subcommands.Add(serveCommand);
+        rootCommand.Subcommands.Add(runCommand);
+        rootCommand.Subcommands.Add(sendClipboardCommand);
+        
+        // Parse and run the root command.
+        var rootCommandParse = rootCommand.Parse(args);
+        if (rootCommandParse.GetValue(DebugOutputOption))
+        {
+            Logger.SetMinimumLogLevel(LogLevel.Debug);
+        }
+        try
+        {
+            rootCommandParse.Invoke();
+        }
+        finally
+        {
+            Logger.WaitForCompletionAsync().Wait();
+        }
+    }
+}
